11c1e12aafee48a476bd3bb737e719ec191ee4d4
void <theprismaticvoid@gmail.com>
2018-09-14 21:50:37 -0500

    Removed default comments/unnecessary pass from _process

--- Game.gd
+++ Game.gd
@@ -6,7 +6,3 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("ui_quit"):
 		get_tree().quit()
-	# Called every frame. Delta is time since last frame.
-	# Update game logic here.
-	pass
-
