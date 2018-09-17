839c569691302a433171aa5a7a8a2103b34bb220
void <theprismaticvoid@gmail.com>
2018-09-16 21:03:35 -0500

    Rudimentary menus added

--- Game.gd
+++ Game.gd
@@ -1,9 +1,7 @@
 extends Node
 
 func _ready():
-	$Sprite.visible=true
-	yield(get_tree().create_timer(.5), "timeout")
-	$Sprite.visible=false
+	
 	pass
 
 func _process(delta):
@@ -11,3 +9,25 @@ func _process(delta):
 		get_tree().quit()
 	pass
 
+func _on_ItemList_item_activated(index):
+	match index:
+		0:
+			_option_new_game()
+		1:
+			_option_continue_game()
+		2:
+			_option_quit_game()
+
+func _option_new_game():
+	print("New game option chosen")
+	get_tree().change_scene("res://Scenes/MainMenu.tscn")
+
+func _option_continue_game():
+	print("Continue game option chosen")
+	
+	#TODO: Add game-loading code here. The call to _option_new_game should be kept as
+	#a last-resort if the player choses Continue without having a save file
+	get_tree().change_scene("res://Scenes/MainMenu.tscn")
+
+func _option_quit_game():
+	get_tree().quit()
