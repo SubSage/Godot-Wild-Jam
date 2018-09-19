ac7ae0a201d03578b3c22fc57c14fb10f02af023
void <theprismaticvoid@gmail.com>
2018-09-18 21:07:21 -0500

    Remove debug code

--- Scripts/Upgrade.gd
+++ Scripts/Upgrade.gd
@@ -13,7 +13,6 @@ onready var spacingHeight = $ItemList.theme.get_constant("Vseparation", "int")
 
 func _process(delta):
 	var newHeight = 0.0
-	newHeight += (fontHeight + spacingHeight) * 2000
-	print(str(newHeight))
+	newHeight += (fontHeight + spacingHeight) * $ItemList.get_item_count() / 2
 	
 	$ItemList.set_anchor(MARGIN_BOTTOM, newHeight)
